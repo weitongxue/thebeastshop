936e93ce29e68a620396d3865b765e06cc19b706
weitongxue <793273237@qq.com>
2018-01-15 22:01:04 +0800

    首页未完成

--- src/components/Head/Header.vue
+++ src/components/Head/Header.vue
@@ -13,13 +13,18 @@ export default {
 </script>
 
 <style lang="less" scoped>
+@import url('../../styles/var.less');
 .head{
   position:fixed;
   top:0;
   left: 0;
   right: 0;
   height: 1.28rem;
-  background-color: red;
+  line-height: 1.28rem;
+  padding: 0 1.6rem;
+  text-align: center;
+  color:@color-text;
+  font-size: .453333333333333rem;
 }
 </style>
 
